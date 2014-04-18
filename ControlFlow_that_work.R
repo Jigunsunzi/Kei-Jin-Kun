219cb9047ea99b8c7d65ca73e33600c644c4790c
Kunthear Ouk <Jigunsunzi@me.com>
2014-04-18 18:38:20 +0200

    some more changes

---
+++ ControlFlow_that_work.R
@@ -0,0 +1,20 @@
+take1<-function(k){
+        k*2
+}
+
+
+take2<-function(k){
+        vir<-k*2
+        print(vir)
+}
+take3<-function(k){
+        vir<-k*2
+        if (k> 3){print(vir)} else {print("nope")}
+}
+
+take4<-function(k){
+        vir<-k*2
+        if (vir>4){print("good")
+} else {
+        print("bad")}
+}
--- first_function_tryout.R
+++
@@ -1,8 +0,0 @@
-take<-function(k){
-        vir<-k*2
-        print(vir)
-}
-take2<-function(k){
-        vir<-k*2
-        if (k> 3){print(vir)} else {print("nope")}
-}
