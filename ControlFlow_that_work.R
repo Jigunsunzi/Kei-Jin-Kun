bc37c2ce36401af8cbb503d3e929bab6a13977ee
Kunthear Ouk <Jigunsunzi@me.com>
2014-04-18 21:45:36 +0200

    123
    :wq

--- ControlFlow_that_work.R
+++ ControlFlow_that_work.R
@@ -18,3 +18,23 @@ take4<-function(k){
 } else {
         print("bad")}
 }
+
+
+
+
+FOR LOOPS
+
+
+for(i in 1:10) {
+        print(x[i])
+}
+
+for(i in seq_along(x)){
+        print(x[i])
+}
+
+for(letter in x){
+        print(letter)
+}
+
+for (i in 1:4) print(x[i])
